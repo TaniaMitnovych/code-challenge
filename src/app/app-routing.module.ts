3b928c11125ec820d4080c8105388c9baa7f621b
TaniaMitnovych <taniamitnovych15@gmail.com>
2023-09-10 18:07:30 +0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
+import { UsersComponent } from './users/users.component';
+import { SavedUsersComponent } from './saved-users/saved-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'weather', component: WeatherComponent },
+  { path: 'saved-users', component: SavedUsersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
